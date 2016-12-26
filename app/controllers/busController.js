0ee391e902401ce9e32997f064a451fceb8047d2
gautampaliwal <gautam.paliwal88@gmail.com>
2016-12-26 23:50:28 +0530

    latest app
    latest app

--- app/controllers/busController.js
+++ app/controllers/busController.js
@@ -33,3 +33,20 @@ app.controller('busController', ['$scope', 'localStorageService', '$location', '
     $scope.GetBusTimings();
    
 }]);
+
+app.filter('unique', function () {
+    return function (collection, keyname) {
+        var output = [],
+            keys = [];
+
+        angular.forEach(collection, function (item) {
+            var key = item[keyname];
+            if (keys.indexOf(key) === -1) {
+                keys.push(key);
+                output.push(item);
+            }
+        });
+
+        return output;
+    };
+});
--- app/controllers/trainController.js
+++ app/controllers/trainController.js
@@ -37,3 +37,21 @@ app.controller('trainController', ['$scope', 'localStorageService', '$location',
     $scope.GetTrainTimings();
    
 }]);
+
+
+app.filter('unique', function () {
+    return function (collection, keyname) {
+        var output = [],
+            keys = [];
+
+        angular.forEach(collection, function (item) {
+            var key = item[keyname];
+            if (keys.indexOf(key) === -1) {
+                keys.push(key);
+                output.push(item);
+            }
+        });
+
+        return output;
+    };
+});
